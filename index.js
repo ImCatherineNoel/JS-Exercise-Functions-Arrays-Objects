d0b7145e3d7d4e9ee14216f23476d0f5b413d8b9
Catherine Tucker <ImCatherineNoel@gmail.com>
2020-05-28 19:04:31 -0700

    Completed: isItAnApple, refactored appleIndex

--- index.js
+++ index.js
@@ -233,195 +233,227 @@ function appleIndex(myFruit){
 
   return(apple);
   } 
+  console.log(appleIndex(['orange','grape','apple','banana','mango']));
+
+*/
+
+/*
+//🟦🟦🟦🟦🟦🟦🟦🟦          My  Solution  #2        🟦🟦🟦🟦🟦🟦🟦🟦 
+function appleIndex(myFruit){ 
+  let fruit = myFruit;
+
+    for (let key in fruit){
+      if (fruit[key] === 'apple'){
+        return(key);
+      }
+      else{ 
+        continue;
+    }
+    }
+  } 
 
 
   console.log(appleIndex(['orange','grape','apple','banana','mango']));
 
 */
 
+/**
+ * ### Challenge `isItAnApple`
+ * 
+ * @instructions
+ * This function takes as its only argument an array 
+ * containing strings,
+ * and returns an array of equal length containing the `true`
+ * if the corresponding entry in the original array is 'apple' 
+ * and `false` if it is anything else.
+ * 
+ * 
+ * For example, if we invoke `isItAnApple`
+ * passing in [ 'orange', 'apple', 'banana', 'apples', 'apple', 'mango' ] as the argument,
+ * the returned value should be: [ false, true, false, false, true, false ].
+*/
+
+/*
+//🟦🟦🟦🟦🟦🟦🟦🟦          My  Solution          🟦🟦🟦🟦🟦🟦🟦🟦      
+
+function isItAnApple(myFruit) {
+  const fruit = myFruit;
+  let output = [];
+ 
+    for (let key in fruit){  {
+      if (fruit[key] === 'apple'){
+        output.push('true');
+      }
+      else {
+        output.push('false');
+      }
+    }
+    
+}
+console.log(output);
+}
+*/
+
+console.log(isItAnApple(['orange', 'apple', 'banana', 'apples', 'apple', 'mango']));
+}
 
 
 
+/*
+// ⭐️ Example Test Data ⭐️
+
+var inventory = [
+  { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+  { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+  { id: 3, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+  { id: 4, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+  { id: 5, car_make: "Mitsubishi", car_model: "Galant", car_year: 1990 },
+  { id: 6, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+  { id: 7, car_make: "Smart", car_model: "Fortwo", car_year: 2009 },
+  { id: 8, car_make: "Audi", car_model: "4000CS Quattro", car_year: 1987 },
+  { id: 9, car_make: "Ford", car_model: "Windstar", car_year: 1996 },
+  { id: 10, car_make: "Mercedes-Benz", car_model: "E-Class", car_year: 2000 },
+  { id: 11, car_make: "Infiniti", car_model: "G35", car_year: 2004 },
+  { id: 12, car_make: "Lotus", car_model: "Esprit", car_year: 2004 },
+  { id: 13, car_make: "Chevrolet", car_model: "Cavalier", car_year: 1997 },
+  { id: 14, car_make: "Dodge", car_model: "Ram Van 1500", car_year: 1999 }
+  /// ... Truncated
+]
+*/
+
+// Data from file 
+var data = require('data/inventory.js'); 
+
+/**
+  * ### Example Array Challenge:
+  * 
+  * @instructions
+  * get3rdCar() should return the string `The is a Land Rover Defender Ice Edition`
+  * 
+  *
+  * NOTE: This example has been completed for you.
+**/
+function get3rdCar(inventory) {
+  const the3rd = inventory[2];
+  return `The is a ${the3rd.car_make} ${the3rd.car_model}`
+}
+
+// 👇 COMPLETE YOUR WORK BELOW 👇
+// 👇 COMPLETE YOUR WORK BELOW 👇
+// 👇 COMPLETE YOUR WORK BELOW 👇
 
 
-// /**
-//  * ### Challenge `isItAnApple`
-//  * 
-//  * @instructions
-//  * This function takes as its only argument an array 
-//  * containing strings,
-//  * and returns an array of equal length containing the `true`
-//  * if the corresponding entry in the original array is 'apple' 
-//  * and `false` if it is anything else.
-//  * 
-//  * 
-//  * For example, if we invoke `isItAnApple`
-//  * passing in [ 'orange', 'apple', 'banana', 'apples', 'apple', 'mango' ] as the argument,
-//  * the returned value should be: [ false, true, false, false, true, false ].
-// */
-// //🟦🟦🟦🟦🟦🟦🟦🟦          My  Solution          🟦🟦🟦🟦🟦🟦🟦🟦      
+/**
+ * ### Challenge `getCarInfoByIndex`
+ * 
+ * @instructions
+ * getCarInfoByIndex takes two arguments:
+ *     (1) an array which is an inventory of cars like the preview above (see ⭐️ Preview Test Data ⭐️)
+ *     (2) a number which is the desired index in the array.
+ * getCarInfoByIndex returns a string in the format `This is a {car_make} {car_model}`
+ * 
+ * For example, if getCarInfoByIndex is invoked with the inventory and the number 0,
+ * it will return `This is a Lincoln Navigator`.
+*/
+function getCarInfoByIndex(inventory, index) {
+  /* code here */
+}
 
-// function isItAnApple(/* code here */) {
-//   /* code here */
-// }
+/**
+ * ### Challenge `getLastCarInfo`
+ * 
+ * @instructions
+ * getLastCarInfo takes a single argument:
+ *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
+ * getLastCarInfo returns a string in the format `This is a {car_make} {car_model}
+ * 
+ * For example, if getLastCarInfo is invoked passing the inventory inside /data/inventory.js,
+ * it will return `This is a Lincoln Town Car`.
+*/
+function getLastCarInfo(/* code here */) {
+  /* code here */
+}
 
+/**
+ * ### Challenge `getModelYears`
+ * 
+ * @instructions
+ * We need the years from every car in the inventory!
+ * getModelYears takes a single argument:
+ *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
+ * getModelYears returns an array containing all the 'car_year's in the inventory.
+*/
+function getModelYears(/* code here */) {
+  /* code here */
+}
 
+/**
+ * ### Challenge `getCarInfoById`
+ *  * * THIS ONE IS A STRETCH GOAL. ATTEMPT IT ONLY AFTER
+ * COMPLETING ALL NON-STRETCH CHALLENGES IN THE REPOSITORY!
+ * 
+ * @instructions
+ * getCarInfoById takes two arguments:
+ *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
+ *     (2) a number which is the desired car id (see how each car has its own unique id).
+ * getCarInfoById returns a string in the format `This is a {car_make} {car_model}
+ * 
+ * For example, if getCarInfoById is invoked with the inventory and the number 1,
+ * it will return `This is a Lincoln Navigator`.
+*/
+function getCarInfoById(/* code here */) {
+  /* code here */
+}
 
-// /*
-// // ⭐️ Example Test Data ⭐️
+/**
+ * ### Challenge `getOlderCars`
+ * * THIS ONE IS A STRETCH GOAL. ATTEMPT IT ONLY AFTER
+ * COMPLETING ALL NON-STRETCH CHALLENGES IN THE REPOSITORY!
+ * 
+ * @instructions
+ * We need a utility to find older cars!
+ * getOlderCars takes two arguments:
+ *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
+ *     (2) a number which is the desired max year.
+ * getOlderCars returns an array containing all the cars
+ * with a `car_year` which is at most the given desired max year,
+ * in the same order as they appear in the original inventory.
+*/
+function getOlderCars(/* code here */) {
+  /* code here */
+}
 
-// var inventory = [
-//   { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
-//   { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
-//   { id: 3, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
-//   { id: 4, car_make: "Honda", car_model: "Accord", car_year: 1983 },
-//   { id: 5, car_make: "Mitsubishi", car_model: "Galant", car_year: 1990 },
-//   { id: 6, car_make: "Honda", car_model: "Accord", car_year: 1995 },
-//   { id: 7, car_make: "Smart", car_model: "Fortwo", car_year: 2009 },
-//   { id: 8, car_make: "Audi", car_model: "4000CS Quattro", car_year: 1987 },
-//   { id: 9, car_make: "Ford", car_model: "Windstar", car_year: 1996 },
-//   { id: 10, car_make: "Mercedes-Benz", car_model: "E-Class", car_year: 2000 },
-//   { id: 11, car_make: "Infiniti", car_model: "G35", car_year: 2004 },
-//   { id: 12, car_make: "Lotus", car_model: "Esprit", car_year: 2004 },
-//   { id: 13, car_make: "Chevrolet", car_model: "Cavalier", car_year: 1997 },
-//   { id: 14, car_make: "Dodge", car_model: "Ram Van 1500", car_year: 1999 }
-//   /// ... Truncated
-// ]
-// */
+/**
+ * ### Challenge `getGermanCars`
+ * * THIS ONE IS A STRETCH GOAL. ATTEMPT IT ONLY AFTER
+ * COMPLETING ALL NON-STRETCH CHALLENGES IN THE REPOSITORY!
+ * 
+ * @instructions
+ * We need a utility to find German cars!
+ * getGermanCars takes a single argument:
+ *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
+ * getGermanCars returns an array containing all the cars
+ * made by either `Audi` or `Mercedes-Benz` or `Volkswagen` or `BMW`,
+ * in the same order as they appear in the original inventory.
+*/
+function getGermanCars(/* code here */) {
+  /* code here */
+}
 
-// // Data from file 
-// //var data = require('data./inventory.js'); 
-
-// /**
-//   * ### Example Array Challenge:
-//   * 
-//   * @instructions
-//   * get3rdCar() should return the string `The is a Land Rover Defender Ice Edition`
-//   * 
-//   *
-//   * NOTE: This example has been completed for you.
-// **/
-// function get3rdCar(inventory) {
-//   const the3rd = inventory[2];
-//   return `The is a ${the3rd.car_make} ${the3rd.car_model}`
-// }
-
-// // 👇 COMPLETE YOUR WORK BELOW 👇
-// // 👇 COMPLETE YOUR WORK BELOW 👇
-// // 👇 COMPLETE YOUR WORK BELOW 👇
-
-
-// /**
-//  * ### Challenge `getCarInfoByIndex`
-//  * 
-//  * @instructions
-//  * getCarInfoByIndex takes two arguments:
-//  *     (1) an array which is an inventory of cars like the preview above (see ⭐️ Preview Test Data ⭐️)
-//  *     (2) a number which is the desired index in the array.
-//  * getCarInfoByIndex returns a string in the format `This is a {car_make} {car_model}`
-//  * 
-//  * For example, if getCarInfoByIndex is invoked with the inventory and the number 0,
-//  * it will return `This is a Lincoln Navigator`.
-// */
-// function getCarInfoByIndex(inventory, index) {
-//   /* code here */
-// }
-
-// /**
-//  * ### Challenge `getLastCarInfo`
-//  * 
-//  * @instructions
-//  * getLastCarInfo takes a single argument:
-//  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
-//  * getLastCarInfo returns a string in the format `This is a {car_make} {car_model}
-//  * 
-//  * For example, if getLastCarInfo is invoked passing the inventory inside /data/inventory.js,
-//  * it will return `This is a Lincoln Town Car`.
-// */
-// function getLastCarInfo(/* code here */) {
-//   /* code here */
-// }
-
-// /**
-//  * ### Challenge `getModelYears`
-//  * 
-//  * @instructions
-//  * We need the years from every car in the inventory!
-//  * getModelYears takes a single argument:
-//  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
-//  * getModelYears returns an array containing all the 'car_year's in the inventory.
-// */
-// function getModelYears(/* code here */) {
-//   /* code here */
-// }
-
-// /**
-//  * ### Challenge `getCarInfoById`
-//  *  * * THIS ONE IS A STRETCH GOAL. ATTEMPT IT ONLY AFTER
-//  * COMPLETING ALL NON-STRETCH CHALLENGES IN THE REPOSITORY!
-//  * 
-//  * @instructions
-//  * getCarInfoById takes two arguments:
-//  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
-//  *     (2) a number which is the desired car id (see how each car has its own unique id).
-//  * getCarInfoById returns a string in the format `This is a {car_make} {car_model}
-//  * 
-//  * For example, if getCarInfoById is invoked with the inventory and the number 1,
-//  * it will return `This is a Lincoln Navigator`.
-// */
-// function getCarInfoById(/* code here */) {
-//   /* code here */
-// }
-
-// /**
-//  * ### Challenge `getOlderCars`
-//  * * THIS ONE IS A STRETCH GOAL. ATTEMPT IT ONLY AFTER
-//  * COMPLETING ALL NON-STRETCH CHALLENGES IN THE REPOSITORY!
-//  * 
-//  * @instructions
-//  * We need a utility to find older cars!
-//  * getOlderCars takes two arguments:
-//  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
-//  *     (2) a number which is the desired max year.
-//  * getOlderCars returns an array containing all the cars
-//  * with a `car_year` which is at most the given desired max year,
-//  * in the same order as they appear in the original inventory.
-// */
-// function getOlderCars(/* code here */) {
-//   /* code here */
-// }
-
-// /**
-//  * ### Challenge `getGermanCars`
-//  * * THIS ONE IS A STRETCH GOAL. ATTEMPT IT ONLY AFTER
-//  * COMPLETING ALL NON-STRETCH CHALLENGES IN THE REPOSITORY!
-//  * 
-//  * @instructions
-//  * We need a utility to find German cars!
-//  * getGermanCars takes a single argument:
-//  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
-//  * getGermanCars returns an array containing all the cars
-//  * made by either `Audi` or `Mercedes-Benz` or `Volkswagen` or `BMW`,
-//  * in the same order as they appear in the original inventory.
-// */
-// function getGermanCars(/* code here */) {
-//   /* code here */
-// }
-
-// /**
-//  * ### Challenge `carMaker`
-//  * THIS ONE IS A STRETCH GOAL. ATTEMPT IT ONLY AFTER
-//  * COMPLETING ALL NON-STRETCH CHALLENGES IN THE REPOSITORY!
-//  * 
-//  * @instructions
-//  * This function takes a single odometer argument (a number) and returns an object.
-//  * The returned object has the following characteristics:
-//  *     it has an `odometer` property that contains the argument passed in.
-//  *     it has a `drive` method that takes a distance as its argument, and
-//  *         (1) causes the odometer in the object to be increased by the distance,
-//  *         (2) returns the updated value of the `odometer`.
-// */
-// function carMaker(/* code here */) {
-//   /* code here */
-// }
+/**
+ * ### Challenge `carMaker`
+ * THIS ONE IS A STRETCH GOAL. ATTEMPT IT ONLY AFTER
+ * COMPLETING ALL NON-STRETCH CHALLENGES IN THE REPOSITORY!
+ * 
+ * @instructions
+ * This function takes a single odometer argument (a number) and returns an object.
+ * The returned object has the following characteristics:
+ *     it has an `odometer` property that contains the argument passed in.
+ *     it has a `drive` method that takes a distance as its argument, and
+ *         (1) causes the odometer in the object to be increased by the distance,
+ *         (2) returns the updated value of the `odometer`.
+*/
+function carMaker(/* code here */) {
+  /* code here */
+}
 
